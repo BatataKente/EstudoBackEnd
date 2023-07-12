8fe1c75cc3ec8ae0f0e736f684302942609e210f
BatataKente <danielpotoco@gmail.com>
2023-07-12 14:51:00 -0300

    first commit

---
+++ src/test/java/com/example/extudoBackEnd/ExtudoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extudoBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtudoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
